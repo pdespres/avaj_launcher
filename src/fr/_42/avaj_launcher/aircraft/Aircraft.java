3f7593fbfb0aa27f77b85f64323e68d7f5c64c0e
pdespres <33380838+pdespres@users.noreply.github.com>
2017-11-26 10:40:53 +0100

    test intellij

---
+++ src/fr/_42/avaj_launcher/aircraft/Aircraft.java
@@ -0,0 +1,19 @@
+package fr._42.avaj_launcher.aircraft;
+
+public abstract class Aircraft {
+
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+    private static long idCounter = 1;
+
+    public Aircraft(String name, Coordinates coordinates) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.id = Aircraft.nextId();
+    }
+
+    private static long nextId(){
+        return(Aircraft.idCounter += 1);
+    }
+}
---
+++ src/fr/_42/avaj_launcher/test.java
@@ -0,0 +1,4 @@
+package fr._42.avaj_launcher;
+
+public class test {
+}
