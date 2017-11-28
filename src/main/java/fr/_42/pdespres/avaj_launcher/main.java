b6b5552cfd55a8b7849ad697b8857f86d4aa0e00
pdespres <pdespres@student.42.fr>
2017-11-28 18:56:32 +0100

    Day 3

--- src/main/java/fr/_42/pdespres/avaj_launcher/main.java
+++ src/main/java/fr/_42/pdespres/avaj_launcher/main.java
@@ -1,8 +1,16 @@
 package fr._42.pdespres.avaj_launcher;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.List;
+
 public class main {
 
     public static void main(String[] args) {
 
+        List<String> lines = Files.readAllLines(FileSystems.getDefault().getPath(args[1]), StandardCharsets.UTF_8);
+
+        // getalgorithm class message digest 128 bits 32 octet
     }
 }
